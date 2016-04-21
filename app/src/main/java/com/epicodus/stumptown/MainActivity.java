87c2fc781591389e5caae0f1d19befa372013eed
chris houghton and trevor elvey <trevor.elvey@gmail.com>
2016-04-21 11:40:31 -0700

    add array adapter and listview

--- app/src/main/java/com/epicodus/stumptown/MainActivity.java
+++ app/src/main/java/com/epicodus/stumptown/MainActivity.java
@@ -2,12 +2,40 @@ package com.epicodus.stumptown;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.AdapterView;
+import android.widget.ArrayAdapter;
+import android.widget.ListView;
+import android.widget.TextView;
+import android.widget.Toast;
 
 public class MainActivity extends AppCompatActivity {
+    private TextView mTextView;
+    private ListView mListView;
+    private String[] coffees = new String[] {"Rwanda Rwamweru", "Rwanda Huye Mountain", "Ethiopia Nano Challa", "Ethiopia Duromina", "Indonesia Bies Penantan", "Colombia El Nevado", "Guatemala Candelaria", "Colombia El Jordan", "Peru Chirinos", "Golden Hour", "Hair Bender", "Trapper Creek Decaf", "Holler Mountain", "House Blend", "French Roast"};
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        mListView = (ListView) findViewById(R.id.listView);
+        ArrayAdapter adapter = new ArrayAdapter(this, android.R.layout.simple_list_item_1, coffees);
+        mListView.setAdapter(adapter);
+
+        mListView.setOnItemClickListener(new AdapterView.OnItemClickListener() {
+            @Override
+            public void onItemClick(AdapterView<?> adapterView, View view, int i, long l) {
+                String coffee = ((TextView)view).getText().toString();
+                Toast.makeText(MainActivity.this, coffee, Toast.LENGTH_LONG).show();
+            }
+        });
+
+
+
+
+
+
+
     }
 }
